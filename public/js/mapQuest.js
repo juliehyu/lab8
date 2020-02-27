6df1f15f5477239993a63dcd96de92890c9362db
Julie Yu <jhy015@ucsd.edu>
2020-02-26 16:01:25 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+    L.mapquest.key = "HP9VdglpF4hRbKDFJ1dQyAA7GqJGMN7P";
+
+    var map = L.mapquest.map("map", {
+        center: [32.88, -117.236],
+        layers: L.mapquest.tileLayer("map"),
+        zoom: 12,
+        zoomControl: false
+    });
+
+    L.marker([32.8788033, -117.2359121]).addTo(map);
 }
